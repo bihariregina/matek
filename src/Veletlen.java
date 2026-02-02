58e76798b959f70b05a09989bd0768adf3ee0dea
Bihari Regina <bihari.regi@gmail.com>
2026-02-02 15:06:36 +0100

    Véletlen szám, kockadobás

---
+++ src/Veletlen.java
@@ -0,0 +1,14 @@
+
+import java.util.Random;
+
+public class Veletlen {
+    public static void csinald() {
+        Random random = new Random();
+        int szam = random.nextInt(3);
+        System.out.println(szam);
+
+        int kocka = random.nextInt(6)+1;
+        System.out.println("kocka: " + kocka);
+    }
+    
+}
